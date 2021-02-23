1079ce566ff357d2012c012c353c21481819c94c
niki-sal <ne.salehi@gmail.com>
2021-02-23 12:38:02 -0800

    using map()

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
     body: "Check out this body property!",
     comments: ["First!", "Great post", "Hire this author now!"]
   }
+  //map returns an array
+  const comments = post.comments.map( comment => (<Comment content = {comment}/>))
+
   return (
     <div className="App">
       {/* can use varaibales with curly brackets */}
@@ -19,7 +22,12 @@ function App() {
       <p>{post.body}</p>
       <h3>Comments:</h3>
       <p>{post.comments[0]}</p>
+      
+      <p>another way of showing a comments(below) by only component</p>
       <Comment name="Nikki" content= "Hello there!" />
+
+      <p>another way of showing all comments(below) by map and component</p>
+      {comments}
     </div>
   );
 }
